c3337251855eeb5c130682071c8ea0ebb4fc43a6
YorgosBardanikas <bardanikas.g@int.univ-amu.fr>
2026-05-06 11:12:56 +0200

    add shell script to run batch jobs

---
+++ submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J batch               # Job name
+#SBATCH -o ./%j.out          # Name of stdout output file (%j expands to jobId)
+#SBATCH -p batch             # Name of partition
+#SBATCH -w niolon14
+#SBATCH -N 1                 # Total number of nodes requested
+#SBATCH -t 23:00:00          # Run time (hh:mm:ss) - 1.5 hours# Launch
+#SBATCH --ntasks-per-node=16 # Number of tasks per node (32 CPUs)
+#SBATCH --mem=75G            # Amount of memory requested
+module purge
+module load all
+module load anaconda/3
+# moving to the working directory
+cd /envau/work/comco/bardanikas.g/Vision4Action/vision4action/analysis_scripts/preprocessing
+source activate gbpy38
+python generate_epochs.py
