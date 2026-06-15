fc64c1b73159a944a9d8031a2c8a0cd1a18db1e2
Georgios BARDANIKAS <georgebarda@gmail.com>
2026-06-15 17:13:52 +0200

    fix variables and comments

--- analysis_scripts/LDA/plot/plot_LDA_projections.py
+++ analysis_scripts/LDA/plot/plot_LDA_projections.py
@@ -7,14 +7,14 @@ from scipy.signal import savgol_filter
 from matplotlib.patches import Polygon
 from frites.stats.stats_nonparam import confidence_interval
 
-subj = 'jazz'
+subj = 'enya'
 onset = 'targ'
 content = 'mua'
 rgr = 'goal'
 area = 'M1'
 plt.rcParams.update({'font.size': 14})
 path = f'{utils.PATH}/LDA_6dirs/'
-
+fig_dir = utils.PATH + '/Figures_revisions/LDA'
 filename = f'{subj}-{onset}-{content}_LDA_LS{rgr}.nc'
 lda = xr.open_dataset(f'{path}{filename}',engine='h5netcdf')
 lda = lda['projections'].sel(areas=area)
@@ -22,14 +22,17 @@ lda['trials'] = lda.true_trials
 trials = lda.trials.data
 times = lda.times*1000
 ntimes = times.data.size
-npcs = lda.PCs.data.size
+nlds = lda.PCs.data.size
+ntargs = 3 # three target ranks
 
 # train: [top-right, right, bottom-right, bottom-left, left, top-left]
 train_code_list = [[32,42],[12,22],[52,62],[112,122],[92,102],[72,82]]
+# test: [top-right, bottom-right, bottom-left, top-left, bottom, top]
 test_code_list3 = [[53,93],[33,103],[23,73],[13,123],[43,83],[63,113]]
 test_code_list4 = [[44,124],[64,74],[34,114],[54,84],[14,94],[24,104]]
 test_codes_list = [test_code_list3, test_code_list4]
 ncl = len(test_codes_list)
+ndirs = len(train_code_list)
 
 train_code_flat = [i for train in train_code_list for i in train]
 train_inds = np.where(np.isin(trials, train_code_flat))[0]
@@ -38,14 +41,14 @@ train_proj = lda.isel(trials=train_inds)
 test_proj = lda.isel(trials=test_inds)
 
 # Plot the train projections across time
-scores_all = np.zeros((npcs,3,6,ntimes)) # ncomps, ntargs, ndirs, ntimes
+scores_all = np.zeros((nlds,ntargs,ndirs,ntimes))
 clrs = ["#5245de","#3c8ac6","#a5bcda","#ec90b0","#ca575e","#c22d3f"]
 clrs2 = ["#5245de","#a5bcda","#ec90b0","#c22d3f","#000000","#838383"]
 t1,t2 = 400,1300
 i = 1
-plt.subplots(npcs,1,sharex=True)
+plt.subplots(nlds,1,sharex=True)
 for s,train in enumerate(train_proj):
-    plt.subplot(npcs,1,i)
+    plt.subplot(nlds,1,i)
 
     for tc,train_codes in enumerate(train_code_list):
         inds = np.where(np.isin(train.trials, train_codes))[0]
@@ -67,11 +70,11 @@ for s,train in enumerate(train_proj):
 
 # Plot the test projections across time
 i = 1
-plt.subplots(npcs,ncl,sharex=True,sharey=True)
+plt.subplots(nlds,ncl,sharex=True,sharey=True)
 for ts,test in enumerate(test_proj):
     for tsc,test_codes in enumerate(test_codes_list):
-        plt.subplot(npcs,ncl,i)
-        for tc,codes in enumerate(test_codes):
+        plt.subplot(nlds,ncl,i)
+        for tc,codes in enumerate(test_codes[:-2]):
             inds = np.where(np.isin(test.trials, codes))[0]
             test_ = test.isel(trials=inds).data
             test_ = savgol_filter(test_,81,1)
@@ -89,24 +92,28 @@ for ts,test in enumerate(test_proj):
         plt.yticks([-5,0,5],['','',''])
         i+=1
 
-# Plot the manifolds at max cross-condition variance
-crossCondVar = scores_all.var(axis=(0,2))  # variance across the 2 LDs and 4 directions -> (ntargs,ntimes)
+# Find the moments of maximum cross-condition variance
+crossCondVar = scores_all.var(axis=(0,2))   # variance across the 2 LDs and 4 directions -> (ntargs,ntimes)
 ccvMax_time = crossCondVar.argmax(axis=-1)  # argmax across times -> (ntargs)
 
-points = np.zeros((3,6,2)) # ntargs,ndirs,ncomps
-for nc in range(2):
-    for nt in range(3):
+# Find the points of max ccv in neural space coordinates
+points = np.zeros((ntargs,ndirs,nlds))
+for nc in range(nlds):
+    for nt in range(ntargs):
         ccv_ = ccvMax_time[nt]
         points[nt,:,nc] = scores_all[nc,nt,:,ccv_]
 
+# Plot the neural space
 plt.figure(figsize=(6,6))
 plt.axis(False)
 
-v = 850 if subj == 'jazz' else 1000
+# Plot the neural trajectory for the center-out movements
+v = 850 if subj == 'jazz' else 900
 for r in range(6):
     plt.plot(scores_all[0,0,r,700:v], scores_all[1,0,r,700:v], 
              c=clrs[r], lw=3) # ncomps, ntargs, ndirs, ntimes
 
+# Plot the points of max ccv per target rank
 ls = ['-','--',':']
 for tg,points_tg in enumerate(points):
         
@@ -133,6 +140,5 @@ for tg,points_tg in enumerate(points):
 #         nd = 4 + n
 #         plt.scatter(points_tg[nd,0], points_tg[nd,1], s=200, c=clrs2[nd])
 
-# fig_dir = f'{utils.v4a_dir}/Figures'
-# plt.savefig(f'{fig_dir}/{subj}_{onset}_LDA_space.svg')
+# plt.savefig(f'{fig_dir}/{subj}_{onset}_LDA_space_topbottom.svg')
 plt.show()
